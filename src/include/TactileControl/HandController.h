c1e7ac2499e938d1ddde1d7959b11165eac0cc6b
Ugo Pattacini <ugo.pattacini@iit.it>
2017-11-28 18:46:25 +0100

    code formatting fixed

--- src/include/TactileControl/HandController.h
+++ src/include/TactileControl/HandController.h
@@ -8,7 +8,7 @@
 /**
  * @authors: Ugo Pattacini <ugo.pattacini@iit.it>
  */
- 
+
 #ifndef LIB_FINGERS_POSITION_CONTROL_H
 #define LIB_FINGERS_POSITION_CONTROL_H
 
@@ -16,33 +16,32 @@
 #include <yarp/os/Value.h>
 #include <iCub/action/actionPrimitives.h>
 
-namespace tactileControl {
+namespace tactileControl { 
 
-class HandController
-{
-	iCub::action::ActionPrimitives *action;
-	std::string hand;
+class HandController {
+  iCub::action::ActionPrimitives* action;
+  std::string hand;
 
-	std::string context;
-	std::string file;
+  std::string context;
+  std::string file;
 
-	bool isOpen() const;
+  bool isOpen() const;
 
-public:
-	HandController();
-	virtual ~HandController();
+ public:
+  HandController();
+  virtual ~HandController();
 
-	virtual bool open();
-	virtual bool close();
+  virtual bool open();
+  virtual bool close();
 
-	virtual bool set(const std::string &context, const std::string &file);
-	virtual bool set(const std::string &key, const yarp::os::Value &value);
+  virtual bool set(const std::string& context, const std::string& file);
+  virtual bool set(const std::string& key, const yarp::os::Value& value);
 
-	virtual bool closeHand(const bool wait = true);
-	virtual bool isHandClose();
+  virtual bool closeHand(const bool wait = true);
+  virtual bool isHandClose();
 
-	virtual bool openHand(const bool fullyOpen, const bool wait = true);
-	virtual bool isHandOpen();
+  virtual bool openHand(const bool fullyOpen, const bool wait = true);
+  virtual bool isHandOpen();
 };
 
 }
